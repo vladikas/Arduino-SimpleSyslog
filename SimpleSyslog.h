f2d7c110159e97a9e8865d6e7a6a71c5340c161c
vladikas <vladikas@mail.ru>
2025-07-04 09:05:20 +0200

    Add support for flash-based strings using F() macro
    ### Summary
    
    This PR adds support for flash-based strings (`__FlashStringHelper*`) passed via the `F()` macro, which is commonly used in Arduino development to reduce RAM usage on ESP-based platforms.
    
    ### What's new
    
    - Added an overload of `SimpleSyslog::printf()` that accepts a `const __FlashStringHelper*` argument.
    - This overload copies the flash string into a temporary buffer and calls the existing `printf(const char*)` method.
    
    ### Why
    
    On ESP platforms, the `F()` macro is often used to store constant strings in flash memory and save heap RAM. This change allows code like the following to work out-of-the-box:
    
    ```cpp
    syslog.printf(FAC_USER, PRI_INFO, F("Connected to Wi-Fi"));

--- SimpleSyslog.h
+++ SimpleSyslog.h
@@ -3,19 +3,18 @@
 
 #define SIMPLE_SYSLOG_VERSION "0.1.3"
 
-#if defined(ESP8266)
-	#include <ESP8266WiFi.h>
-#elif defined(ESP32)
-	#include <WiFi.h>
+#if defined (ESP8266)
+  #include <ESP8266WiFi.h>
+  #include <pgmspace.h>
+#elif defined (ESP32)
+  #include <WiFi.h>
+  #include <pgmspace.h>
 #else
-	#error SimpleSyslog requires an ESP board. Disabling for non-supported board
+  #error SimpleSyslog requires an ESP board. Disabling for non-supported board
 #endif
-
-// Syslog reference: https://www.dl8rds.de/index.php/Arduino_Syslog_Client_Library
 
 #include <Arduino.h>
 #include <WiFiUdp.h>
-
 
 #define PRI_EMERGENCY 0
 #define PRI_ALERT     1
@@ -26,55 +25,71 @@
 #define PRI_INFO      6
 #define PRI_DEBUG     7
 
-#define FAC_USER   1
-#define FAC_LOCAL0 16
-#define FAC_LOCAL1 17
-#define FAC_LOCAL2 18
-#define FAC_LOCAL3 19
-#define FAC_LOCAL4 20
-#define FAC_LOCAL5 21
-#define FAC_LOCAL6 22
-#define FAC_LOCAL7 23
+#define FAC_USER      1
+#define FAC_LOCAL0    16
+#define FAC_LOCAL1    17
+#define FAC_LOCAL2    18
+#define FAC_LOCAL3    19
+#define FAC_LOCAL4    20
+#define FAC_LOCAL5    21
+#define FAC_LOCAL6    22
+#define FAC_LOCAL7    23
 
 class SimpleSyslog {
-	WiFiUDP SimpleSyslog_udp;
+  WiFiUDP SimpleSyslog_udp;
 
-	public:
-		SimpleSyslog(const char* hostname, const char* app, const char* server, uint16_t port = 514, uint16_t max_packet_size = 128) {
-			this->_hostname        = hostname;
-			this->_app             = app;
-			this->_server          = server;
-			this->_port            = port;
-			this->_max_packet_size = max_packet_size;
-		}
+public:
+  SimpleSyslog(const char* hostname, const char* app, const char* server, uint16_t port = 514, uint16_t max_packet_size = 128) {
+	this->_hostname        = hostname;
+	this->_app             = app;
+	this->_server          = server;
+	this->_port            = port;
+	this->_max_packet_size = max_packet_size;
 
-		void printf(uint8_t facility, uint8_t severity, const char* format, ...) {
-			// The PRI value is an integer number which calculates by the following metric:
-			uint8_t priority = (8 * facility) + severity;
+  }
 
-			// Get the variadic params from this function
-			va_list args;
-			va_start(args, format);
-			char buf[_max_packet_size];
-			vsnprintf(buf, _max_packet_size, format, args);
-			va_end(args);
+  // strings from RAM
+  void printf(uint8_t facility, uint8_t severity, const char* format, ...) {
+    uint8_t priority = (8 * facility) + severity;
 
-			// This is a unit8 instead of a char because that's what udp.write() wants
-			uint8_t buffer[_max_packet_size];
-			int len = snprintf((char*)buffer, _max_packet_size, "<%d>%s %s: %s", priority, _hostname, _app, buf);
+    va_list args;
+    va_start(args, format);
+    char buf[_max_packet_size];
+    vsnprintf(buf, _max_packet_size, format, args);
+    va_end(args);
 
-			// Send the raw UDP packet
-			SimpleSyslog_udp.beginPacket(_server, _port);
-			SimpleSyslog_udp.write(buffer, len);
-			SimpleSyslog_udp.endPacket();
-		}
+    uint8_t buffer[_max_packet_size];
+    int len = snprintf((char*)buffer, _max_packet_size, "<%d>%s %s: %s", priority, _hostname, _app, buf);
 
-	private:
-		const char* _hostname;
-		const char* _app;
-		const char* _server;
-		uint16_t _port;
-		uint16_t _max_packet_size;
+    SimpleSyslog_udp.beginPacket(_server, _port);
+    SimpleSyslog_udp.write(buffer, len);
+    SimpleSyslog_udp.endPacket();
+  }
 
+  // strings from F()
+  void printf(uint8_t facility, uint8_t severity, const __FlashStringHelper* format, ...) {
+    uint8_t priority = (8 * facility) + severity;
+
+    va_list args;
+    va_start(args, format);
+    char buf[_max_packet_size];
+    vsnprintf_P(buf, _max_packet_size, (PGM_P)format, args);
+    va_end(args);
+
+    uint8_t buffer[_max_packet_size];
+    int len = snprintf((char*)buffer, _max_packet_size, "<%d>%s %s: %s", priority, _hostname, _app, buf);
+
+    SimpleSyslog_udp.beginPacket(_server, _port);
+    SimpleSyslog_udp.write(buffer, len);
+    SimpleSyslog_udp.endPacket();
+  }
+
+private:
+  const char* _hostname;
+  const char* _app;
+  const char* _server;
+  uint16_t _port;
+  uint16_t _max_packet_size;
 };
+
 #endif
